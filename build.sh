a8436ccd0a876c6a8b41a49b8ace4ad623512da3
Ben Cessa <ben@pixative.com>
2022-01-05 11:14:45 -0600

    Buf v1.0.0-rc10

--- build.sh
+++ build.sh
@@ -6,21 +6,25 @@ IMAGE_REGISTRY=ghcr.io/bryk-io
 if [ -z "$1" ]; then
   for img in *; do
     if [ -d "$img" ]; then
+      cp ca-roots.crt "$img"/ca-roots.crt
       VERSION=$(cat "$img"/version)
       docker rmi "${IMAGE_REGISTRY}/${img}:${VERSION}"
       docker build \
       --build-arg VERSION="$VERSION" \
       -t "${IMAGE_REGISTRY}/${img}:${VERSION}" \
       -f "$img"/Dockerfile "$img"/.
+      rm "$img"/ca-roots.crt
     fi
   done
   exit 0
 fi
 
 # Build only specified image
+cp ca-roots.crt "$1"/ca-roots.crt
 VERSION=$(cat "$1"/version)
 docker rmi "${IMAGE_REGISTRY}/$1:${VERSION}"
 docker build \
 --build-arg VERSION="$VERSION" \
 -t "${IMAGE_REGISTRY}/$1:${VERSION}" \
 -f "$1"/Dockerfile "$1"/.
+rm "$1"/ca-roots.crt
